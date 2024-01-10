0fe109e4d8d1ff8a38db9dea2529122c6a075ecb
todorz4 <zagoractodor@gmail.com>
2024-01-11 00:26:16 +0100

    Whate

---
+++ admin.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+include 'conn/connection.php';
+include 'functions/functions.php';
+
+$kor_ime=$_POST["kor_ime"];
+$lozinka=$_POST["lozinka"];
+
+
+var_dump($kor_ime,$lozinka);
+
+
+
+
+
+
+?>
---
+++ proizvodi.php
@@ -0,0 +1,18 @@
+<?php
+include 'header.php';
+include 'conn/connection.php';
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
